4011fc04e822c35d3762737db19d53a716c05ecf
Reaplay <terran88@gmail.com>
2016-04-18 12:12:43 +0400

    Update search.php

--- search.php
+++ search.php
@@ -17,14 +17,25 @@ id - идентификатор некоторых значений
 
 */
 
-    if ($_GET['s']){
+     if ($_GET['s']){
+		if($_GET['type']=="card"){
+			$res = sql_query("
+SELECT card_client.name, card_client.id, card_client.mobile, card_client.equid, department.name as d_name, users.name as u_name
+FROM `card_client`
+LEFT JOIN department ON department.id = card_client.department
+LEFT JOIN users ON users.id = card_client.id_manager
+WHERE card_client.name LIKE '%".$_GET['s']."%' OR card_client.mobile LIKE '%".$_GET['s']."%' OR card_client.equid LIKE '%".$_GET['s']."%' LIMIT 0 , 30")  or sqlerr(__FILE__,__LINE__);
+		}
+		else{
         $res = sql_query("
-SELECT client.name, client.mobile, client.equid, department.name as d_name, users.name as u_name
+SELECT client.name, card_client.id, client.mobile, client.equid, department.name as d_name, users.name as u_name
 FROM `client`
 LEFT JOIN department ON department.id = client.department
 LEFT JOIN users ON users.id = client.manager
 WHERE client.name LIKE '%".$_GET['s']."%' OR client.mobile LIKE '%".$_GET['s']."%' OR client.equid LIKE '%".$_GET['s']."%' LIMIT 0 , 30")  or sqlerr(__FILE__,__LINE__);
-        if(mysql_num_rows($res) == 0){
+		}
+		
+		if(mysql_num_rows($res) == 0){
             stderr("Ошибка","Ничего не найдено","no");
         }
 

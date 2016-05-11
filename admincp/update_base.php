3c3d32a527d0db46888838421508bdf1f65b90a6
Reaplay <terran88@gmail.com>
2016-05-12 00:07:54 +0400

    Добавлен новый фикс

--- admincp/update_base.php
+++ admincp/update_base.php
@@ -15,6 +15,7 @@
         /*$res = sql_query("SELECT * FROM sys_fix_base WHERE id = '".$_GET['id']."'");
         $row = mysql_fetch_array($res);
         */
+        // прикрепляем к клиенту ID последнего колбека
         if($_GET['type'] == 'fix_callback_1') {
             $res = sql_query ("SELECT `id`,`id_client` FROM callback WHERE status = '0';");
            if (mysql_num_rows ($res) == 0) {
@@ -24,6 +25,18 @@
 
                 sql_query("UPDATE client SET id_callback = '".$row['id']."' WHERE id = '".$row['id_client']."'");
                 //UPDATE cache_stats SET cache_value=".sqlesc($_POST[$param])." WHERE cache_name='$param'";
+            }
+        }
+        // прикрепляем к клиенту дату последнего колбека
+        if($_GET['type'] == 'fix_callback_2') {
+            $res = sql_query ("SELECT `id`,`id_client`, `next_call` FROM callback WHERE status = '0';");
+            if (mysql_num_rows ($res) == 0) {
+                stderr ("Ошибка", "Данный фикс не требуется", "no");
+            }
+            while ($row = mysql_fetch_array ($res)) {
+
+                sql_query("UPDATE client SET `next_call` = '".$row['next_call']."' WHERE id = '".$row['id_client']."'");
+
             }
         }
     }

fa02b282b5ed0c7bd81ed828fda3fddbf1c95ec4
Reaplay <terran88@gmail.com>
2016-05-11 23:44:40 +0400

    Небольшие изменения в логике

--- elements/client/add_change_client.php
+++ elements/client/add_change_client.php
@@ -138,16 +138,16 @@ INSERT INTO `client`(
 `name`, `department`, `manager`, `mobile`, `email`, `birthday`, `gender`, `added`, `who_added`, `equid`, `comment`,`status`,`vip`)
 VALUES (".implode(",", array_map("sqlesc", array(
 			$name, $department, $manager, $mobile, $email, $birthday, $gender, time(), $CURUSER['id'],$equid, $comment, $status, $vip))).");")  or sqlerr(__FILE__, __LINE__);
-	$id = mysql_insert_id();
-	write_log("Добавлен клиент ID:".$id."","client","add" );
+	$id_client = mysql_insert_id();
+	write_log("Добавлен клиент ID:".$id_client."","client","add" );
 }
 else {
 sql_query("
 UPDATE `client` SET `name` = '".$name."', `manager` = '".$manager."', `department` = '".$department."', `mobile` = '".$mobile."', `email` = '".$email."',`birthday` = '".$birthday."', `gender` = '".$gender."', `comment` = '".$comment."', `last_update` = '".time()."', `status` = '".$status."', `vip`='".$vip."' WHERE `id` ='".$id."';")  or sqlerr(__FILE__, __LINE__);
 	write_log("Изменен клиент ID:".$id."","client","edit" );
 }
-stdmsg("Выполнено","Вы будете перенаправлены на страницу клиента через пару секунд. <br /> Если этого не произошло, нажмите <a href=\"client.php?a=view&id=".$id."\">здесь</a>");
+stdmsg("Выполнено","Вы будете перенаправлены на страницу клиента через пару секунд. <br /> Если этого не произошло, нажмите <a href=\"client.php?a=view&id=".$id_client."\">здесь</a>");
 
-	safe_redirect("client.php?a=view&id=".$id."",1);
+	safe_redirect("client.php?a=view&id=".$id_client."",1);
 
 ?>
